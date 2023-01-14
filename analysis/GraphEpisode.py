Updated graphing and rendering with more data and improved looks. Fixed network to build statically. The old build was preventing gradients from being computed correctly

analysis/GraphEpisode.py
@@ -26,20 +26,14 @@ def graph_episode(k, df, trial_group, trial_number, file_name):
     fig = plt.figure(0, figsize=(14, 10))
     spec = gridspec.GridSpec(ncols=4, nrows=4, figure=fig)
     ax1 = fig.add_subplot(spec[0:2, 0:2])
-    ax2 = fig.add_subplot(spec[0, 2])
-    ax3 = fig.add_subplot(spec[0, 3])
-    ax4 = fig.add_subplot(spec[1, 2])
-    ax5 = fig.add_subplot(spec[1, 3])
+    ax2 = fig.add_subplot(spec[0:2, 2:4])
 
-    ax6 = fig.add_subplot(spec[2, 0])
-    ax7 = fig.add_subplot(spec[2, 1])
-    ax8 = fig.add_subplot(spec[2, 2])
-    ax9 = fig.add_subplot(spec[2, 3])
+    ax3 = fig.add_subplot(spec[2:4, 0:2])
+    ax4 = fig.add_subplot(spec[2, 2])
+    ax5 = fig.add_subplot(spec[2, 3])
 
-    ax10 = fig.add_subplot(spec[3, 0])
-    ax11 = fig.add_subplot(spec[3, 1])
-    ax12 = fig.add_subplot(spec[3, 2])
-    ax13 = fig.add_subplot(spec[3, 3])
+    ax6 = fig.add_subplot(spec[3, 2])
+    ax7 = fig.add_subplot(spec[3, 3])
 
 
     # graph trajectory
@@ -59,7 +53,52 @@ def graph_episode(k, df, trial_group, trial_number, file_name):
     ax1.set_xlabel('x [m]')
     ax1.set_ylabel('y [m]')
 
+    # graph q values
+    q_val_header = [n for n in list(df.columns) if 'Network_output' in n]
+    df_q_vals = df[q_val_header]
+    df_q_vals['mean'] = df_q_vals.mean(axis=1)
+    df_q_vals['min'] = df_q_vals.min(axis=1)
+    df_q_vals['max'] = df_q_vals.max(axis=1)
+    ax2.plot(df['time'].values,df_q_vals['max'],label='max')
+    ax2.plot(df['time'].values, df_q_vals['min'], label='min')
+    ax2.plot(df['time'].values, df_q_vals['mean'], label='mean')
+    ax2.legend()
+    ax2.set_xlabel('Time [s]')
+    ax2.set_ylabel('Q value')
 
+    # graph angles
+    ax3.plot(df['time'].values,np.rad2deg(df['psi'].values),label='$\phi$ hull angle')
+    ax3.plot(df['time'].values, np.rad2deg(df['delta'].values), label='$\delta$ propeller angle')
+    ax3.plot(df['time'].values, np.rad2deg(df['mu'].values), label='$\mu$ angle to dest')
+    ax3.plot([0,max(df['time'].values)],[45,45],'k--',label='Max Propeller')
+    ax3.plot([0, max(df['time'].values)], [-45, -45], 'k--')
+    ax3.legend()
+    ax3.set_xlabel('Time [s]')
+    ax3.set_ylabel('Angle [deg]')
+
+    # reward
+    ax4.plot(df['time'].values, df['reward'].values)
+    ax4.set_xlabel('Time [s]')
+    ax4.set_ylabel('Reward [-]')
+
+    # velocities
+    ax5.plot(df['time'].values, df['v_xp'].values,label='x_p')
+    ax5.plot(df['time'].values, df['v_yp'].values,label='y_p')
+    ax5.legend()
+    ax5.set_xlabel('Time [s]')
+    ax5.set_ylabel('Velocities [m/s]')
+
+    # velocities
+    ax6.plot(df['time'].values, df['dest_dist'].values)
+    ax6.plot([0, max(df['time'].values)], [5.0,5.0], label='Threshold')
+    ax6.legend()
+    ax6.set_xlabel('Time [s]')
+    ax6.set_ylabel('Distance to Destination [m]')
+
+    # fuel
+    ax7.plot(df['time'].values, df['fuel'].values)
+    ax7.set_xlabel('Time [s]')
+    ax7.set_ylabel('Fuel [kg]')
 
     plt.suptitle('Trajectories for Episode=' + str(episode) + ' Trial Group=' + str(
         trial_group) + ' Trial Number=' + str(trial_number))
@@ -73,9 +112,9 @@ if __name__ == '__main__':
     # Edit this block to control what is rendered ----------------------------------------------------------------------
     # ------------------------------------------------------------------------------------------------------------------
     # input controls.
-    trial_group = 'Debugging'
-    trial_number = 0
-    episodes = [1998]  # range(20)
+    trial_group = 'DebuggingDirectControl'
+    trial_number = 7
+    episodes = range(500)
 
     # ------------------------------------------------------------------------------------------------------------------
     # Edit this block to control what is rendered ----------------------------------------------------------------------

src/Environment.py
@@ -204,6 +204,7 @@ class Environment(ABC):
                 next_state = interim_next_state
 
                 # convert the tuple to tensors
+                #tmp = list(state.values())
                 state_tensor = ReplayMemory.convert_numpy_to_tensor(self.device,list(state.values()))
                 next_state_tensor = ReplayMemory.convert_numpy_to_tensor(self.device, list(next_state.values()))
                 #action_tensor = torch.tensor([action]).to(self.device)
@@ -264,6 +265,16 @@ class Environment(ABC):
 
             # add history together
             total_history = pd.concat([total_history, tmp_histroy], axis=1)
+
+        # add data from learning agent
+        la_history = self.agent.output_history
+        la_history = np.reshape(la_history,(len(la_history),len(la_history[0])))
+        columns = []
+        for i in range(len(la_history[0])):
+            columns.append('Network_output_'+str(i))
+        la_history = pd.DataFrame(data=la_history,columns=columns)
+        total_history = pd.concat([total_history,la_history], axis=1)
+        self.agent.reset_output_history()
 
         # write total history out to a file
         file_name = 'Output//' + str(self.h_params['scenario']['experiment_set'])+ '//' + str(self.h_params['scenario']['trial_num'])+'//TrainingHistory//Data//History_'+str(ep_num)+'.csv'
@@ -468,7 +479,8 @@ class Environment(ABC):
             if elapsed_episodes > 0 and elapsed_episodes % self.h_params['learning_algorithm']['target_frequency'] == 0:
 
                 # update the target networks parameters
-                self.agent.target_network.load_state_dict(self.agent.network.state_dict())
+                #self.agent.target_network.load_state_dict(self.agent.network.state_dict())
+                self.agent.update_target_network()
 
                 # save the networks
                 torch.save(self.agent.network.state_dict(),
@@ -531,45 +543,48 @@ class Environment(ABC):
         :return: none
         """
 
-        sensors_data = self.h_params['sensors']
+        try:
+            sensors_data = self.h_params['sensors']
 
-        for name, sensor in sensors_data.items():
+            for name, sensor in sensors_data.items():
 
-            if 'lidar_' in name:
-                # create a ProcessedLidar object that simulates the measurements after the raw lidar data has been
-                # filtered and obstacles more succintly defined
+                if 'lidar_' in name:
+                    # create a ProcessedLidar object that simulates the measurements after the raw lidar data has been
+                    # filtered and obstacles more succintly defined
 
-                measurement_df = pd.DataFrame(columns=['name','norm_value','norm_method'])
-                meas_var_info = sensor['max_range']
-                meas_var_info = meas_var_info.split(',')
-                row = dict()
-                row['name'] = 'max_range'
-                row['norm_value'] = float(meas_var_info[1])
-                row['norm_method'] = meas_var_info[2]
-                measurement_df = measurement_df.append(row, ignore_index=True)
+                    measurement_df = pd.DataFrame(columns=['name','norm_value','norm_method'])
+                    meas_var_info = sensor['max_range']
+                    meas_var_info = meas_var_info.split(',')
+                    row = dict()
+                    row['name'] = 'max_range'
+                    row['norm_value'] = float(meas_var_info[1])
+                    row['norm_method'] = meas_var_info[2]
+                    measurement_df = measurement_df.append(row, ignore_index=True)
 
-                meas_var_info = sensor['max_theta']
-                meas_var_info = meas_var_info.split(',')
-                row = dict()
-                row['name'] = 'max_theta'
-                row['norm_value'] = float(meas_var_info[1])
-                row['norm_method'] = meas_var_info[2]
-                measurement_df = measurement_df.append(row, ignore_index=True)
+                    meas_var_info = sensor['max_theta']
+                    meas_var_info = meas_var_info.split(',')
+                    row = dict()
+                    row['name'] = 'max_theta'
+                    row['norm_value'] = float(meas_var_info[1])
+                    row['norm_method'] = meas_var_info[2]
+                    measurement_df = measurement_df.append(row, ignore_index=True)
 
-                tmp_sensor = Sensors.ProcessedLidar(base_range=float(sensor['base_range']),base_theta=float(sensor['base_theta']),
-                                       name=name,measurement_norm_df=measurement_df,mover_owner_name=sensor['install_on'])
+                    tmp_sensor = Sensors.ProcessedLidar(base_range=float(sensor['base_range']),base_theta=float(sensor['base_theta']),
+                                           name=name,measurement_norm_df=measurement_df,mover_owner_name=sensor['install_on'])
 
-                for mover_name, mover in self.mover_dict.items():
-                    if mover_name == tmp_sensor.mover_owner_name:
-                        mover.add_sensor(tmp_sensor)
+                    for mover_name, mover in self.mover_dict.items():
+                        if mover_name == tmp_sensor.mover_owner_name:
+                            mover.add_sensor(tmp_sensor)
 
-                        # add the data from the sensor to the history for the mover
-                        tmp_state = tmp_sensor.get_raw_measurements()
-                        for ts in tmp_state:
-                            mover.history_header.append(mover.state_dict['name'] + '_' + ts)
+                            # add the data from the sensor to the history for the mover
+                            tmp_state = tmp_sensor.get_raw_measurements()
+                            for ts in tmp_state:
+                                mover.history_header.append(mover.state_dict['name'] + '_' + ts)
 
-            else:
-                raise ValueError('Sensor not currently supported')
+                else:
+                    raise ValueError('Sensor not currently supported')
+        except:
+            pass
 
     def add_entities(self):
         """

src/LearningAlgorithms.py
@@ -28,7 +28,7 @@ class Network(nn.Module):
         :param state_size: the number of inputs for the neural network
         """
         super(Network, self).__init__()
-
+        '''
         # build the input and hidden layers
         self.layers = []
         for i, layer in enumerate(layer_numbers):
@@ -67,13 +67,21 @@ class Network(nn.Module):
 
         # prepare dropout layers
         self.drop = nn.Dropout(h_params['learning_algorithm']['drop_out'])
+        '''
+        self.fc1 = nn.Linear(state_size, 100, device=device)
+        self.fc2 = nn.Linear(100,100, device=device)
+        self.out = nn.Linear(100, action_size, device=device)
+        self.relu = torch.nn.ReLU()
+        #self.drop = nn.Dropout(h_params['learning_agent']['drop_out'])
 
-    def forward(self, x):
+    def forward(self, z):
         """
         Override the forward function to generate an ouput from the network
 
         :param x: input to the network. Should be a state vector that is normalized
         :return: the output of the network
+        """
+
         """
         for layer in self.layers:
             x = layer(x)
@@ -85,6 +93,14 @@ class Network(nn.Module):
         # if not a linear output
         if self.last_active is not None:
             x = self.last_active(x)
+        """
+
+        x = self.fc1(z)
+        x = self.relu(x)
+        x = self.fc2(x)
+        #x = self.drop(x)
+        x = self.relu(x)
+        x = self.out(x)
 
         return x
 
@@ -124,6 +140,15 @@ class LearningAlgorithms:
         self.gamma = h_params['learning_algorithm']['gamma']
         self.device = device
         self.optimizer = None
+
+        self.output_history = []
+
+    def reset_output_history(self):
+        """
+        Reset stored values from the output of the network
+        :return:
+        """
+        self.output_history = []
 
 
 class DQN(LearningAlgorithms):
@@ -165,10 +190,14 @@ class DQN(LearningAlgorithms):
         """
         inp = ReplayMemory.convert_numpy_to_tensor(self.device, inp)
         if is_grad:
-            return self.network.forward(torch.Tensor(inp))
+            out = self.network.forward(torch.Tensor(inp))
+            #self.output_history.append(out.to('cpu').numpy()[0])
+            return out
         else:
             with torch.no_grad():
-                return self.network.forward(torch.Tensor(inp))
+                out = self.network.forward(torch.Tensor(inp))
+                self.output_history.append(out.to('cpu').numpy()[0])
+                return out
 
     def train_agent(self, replay_storage):
         """
@@ -197,11 +226,15 @@ class DQN(LearningAlgorithms):
 
             # use the policy network
             #state_action_values = self.network(state_batch).gather(1, action_batch.type(torch.int64))
-            tmp_out = self.network(state_batch)
-            state_action_values = torch.gather(tmp_out,1,action_batch.type(torch.int64))
+            #tmp_out = self.network(state_batch)
+            #tmp_2 = self.network(state_batch).gather(1, action_batch.type(torch.int64))
+            #state_action_values = torch.gather(tmp_out,1,action_batch.type(torch.int64))
+            state_action_values = self.network(state_batch).gather(1, action_batch.type(torch.int64))
 
             #next_state_values = torch.zeros(self.h_params['learning_agent']['batch_size'], device=self.device)
             next_state_values = torch.zeros(self.batch_size, device=self.device)
+            #test = torch.zeros(self.batch_size, device=self.device)
+            #test[non_final_mask] = self.network(non_final_next_states).max(1)[0].detach()
             next_state_values[non_final_mask] = self.target_network(non_final_next_states).max(1)[0].detach()
 
             expected_state_action_values = torch.add(

src/RewardFunctions.py
@@ -202,7 +202,7 @@ class InstantStepHeadingCrashSuccessReward(RewardFunction):
         self.crash_reward = crash_reward
         self.success_reward = success_reward
         self.heading_old = None
-        self.heading_reward = 0.025
+        self.heading_reward = 0.075
 
     def get_reward(self, t, mover_dict):
         """
@@ -222,7 +222,7 @@ class InstantStepHeadingCrashSuccessReward(RewardFunction):
                 delta_range = self.old_dst - mover.state_dict['dest_dist']
                 if delta_range >= 0:
                     # positive reward if boat got closer to the goal
-                    reward += delta_range/(10.0*self.delta_t)
+                    reward += delta_range/(1.0*self.delta_t)
 
                 self.old_dst = mover.state_dict['dest_dist']
 
@@ -232,12 +232,14 @@ class InstantStepHeadingCrashSuccessReward(RewardFunction):
                     reward += self.success_reward
 
                 # Check if heading is point boat more towards
-                if np.abs(mover.state_dict['mu']) < np.abs(self.heading_old) or np.abs(mover.state_dict['mu']) <= np.deg2rad(5.0):
+                if np.abs(mover.state_dict['mu']) < np.abs(self.heading_old):
                     reward += (np.pi - abs(mover.state_dict['mu'])) * (np.pi - abs(mover.state_dict['mu'])) * self.heading_reward
-                else:
-                    # penalty for turning away from the destination
-                    reward -= (np.pi - abs(mover.state_dict['mu'])) * (
-                                np.pi - abs(mover.state_dict['mu'])) * self.heading_reward
+                if np.abs(mover.state_dict['mu']) <= np.deg2rad(5.0):
+                    reward += 1.0
+                #else:
+                #    # penalty for turning away from the destination
+                #    reward -= (np.pi - abs(mover.state_dict['mu'])) * (
+                #                np.pi - abs(mover.state_dict['mu'])) * self.heading_reward
                 self.heading_old = mover.state_dict['mu']
 
                 # check if crashed
@@ -261,7 +263,7 @@ class InstantStepHeadingCrashSuccessReward(RewardFunction):
 
         # if the boat is over 300 meters away from the goal, end the simulation. It is assumed that if the boat goes
         # too far it will not return and this saves computation
-        if self.old_dst >= 300.0:
+        if self.old_dst >= 200.0:
             self.is_terminal = True
 
         return reward